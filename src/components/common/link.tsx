3e5711db68e0c8a25e43936e1207387c32c58770
tshrusd <tshr.usd@gmail.com>
2023-06-07 21:43:41 +0900

    add link component

---
+++ src/components/common/link.tsx
@@ -0,0 +1,20 @@
+import NextLink, { LinkProps as NextLinkProps } from 'next/link'
+import React from 'react'
+
+const style: React.CSSProperties = {
+  textDecoration: 'none',
+}
+
+type LinkProps = {
+  href: NextLinkProps['href']
+  target?: string
+  children?: React.ReactNode
+}
+
+const MyLink: React.FC<LinkProps> = (props) => (
+  <NextLink href={props.href} legacyBehavior>
+    <a style={style}>{props.children}</a>
+  </NextLink>
+)
+
+export default MyLink
