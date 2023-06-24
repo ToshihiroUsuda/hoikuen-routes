fe836c3ef87739130a1ea1c50a73e7ee26c82236
tshrusd <tshr.usd@gmail.com>
2023-06-24 21:52:21 +0900

    scroll to result

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Box } from '@mui/material'
@@ -13,6 +13,12 @@ const Home: NextPage = () => {
   const onSubmit = (data: FormValues) => {
     setSearchParams(data)
   }
+  const resultsViewRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (searchParams && resultsViewRef && resultsViewRef.current) {
+      resultsViewRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+  }, [searchParams])
 
   return (
     <Layout home>
@@ -22,7 +28,12 @@ const Home: NextPage = () => {
       <Box pt={16}>
         <InputForm onSubmit={onSubmit} />
       </Box>
-      {searchParams && <ResultsView searchParams={searchParams} />}
+      {searchParams && (
+        <div ref={resultsViewRef}>
+          <ResultsView searchParams={searchParams} />
+          <div />
+        </div>
+      )}
     </Layout>
   )
 }
